06507111c02ce96c420f786068bb6d588a673b10
Kwabena Boohene <okwabse@gmail.com>
2018-04-11 14:06:55 +0000

    Added on pulse animation to social icons on hover

--- js/transitions.js
+++ js/transitions.js
@@ -11,11 +11,22 @@ $("#scroll").click(function(e) {
   );
 });
 
+//Added pulse animation on hover to social icons
+$("[id^=social]").hover(
+  function() {
+    $(this).addClass("animated infinite pulse");
+  },
+  function() {
+    $(this).removeClass("animated infinite pulse");
+  }
+);
+
 //Fades text into page
 function fadeIn(id) {
   $("#" + id).addClass("animated fadeInRight");
 }
 
+//Timer on when to load the animation
 setTimeout(function() {
   fadeIn("desc1");
   fadeIn("desc2");
