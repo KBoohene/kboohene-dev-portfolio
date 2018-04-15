293deacaf0b93b32f6f1ab49960141f557903f78
Kwabena Boohene <okwabse@gmail.com>
2018-04-15 01:31:37 +0000

    Added nav bar which hides on scroll

--- js/transitions.js
+++ js/transitions.js
@@ -1,8 +1,9 @@
 //Arrow animation for page scrolling
-$("#scroll").click(function (e) {
+$("#scroll").click(function(e) {
   e.preventDefault();
   var position = $(".cool-dev").offset().top;
-  $("body, html").animate({
+  $("body, html").animate(
+    {
       scrollTop: position
     },
     1200
@@ -12,10 +13,11 @@ $("#scroll").click(function (e) {
 /*Scoll animation for different taglines*/
 
 //cool-dev
-$("#tag1").click(function (e) {
+$("#tag1").click(function(e) {
   e.preventDefault();
   var position = $(".cool-dev").offset().top;
-  $("body, html").animate({
+  $("body, html").animate(
+    {
       scrollTop: position
     },
     1200
@@ -23,10 +25,11 @@ $("#tag1").click(function (e) {
 });
 
 //code-fanatic
-$("#tag2").click(function (e) {
+$("#tag2").click(function(e) {
   e.preventDefault();
   var position = $(".code-fan").offset().top;
-  $("body, html").animate({
+  $("body, html").animate(
+    {
       scrollTop: position
     },
     1500
@@ -34,10 +37,11 @@ $("#tag2").click(function (e) {
 });
 
 //react-buff
-$("#tag3").click(function (e) {
+$("#tag3").click(function(e) {
   e.preventDefault();
   var position = $(".react-buff").offset().top;
-  $("body, html").animate({
+  $("body, html").animate(
+    {
       scrollTop: position
     },
     1800
@@ -45,32 +49,53 @@ $("#tag3").click(function (e) {
 });
 
 //Scroll to top button
-$("#arrow-up").click(function () {
-  $("html, body").animate({
-    scrollTop: 0
-  }, 1800);
+$("#arrow-up").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    1800
+  );
   return false;
 });
 
 //Added pulse animation on hover to social icons
 $("[id^=social]").hover(
-  function () {
+  function() {
     $(this).addClass("animated infinite pulse");
   },
-  function () {
+  function() {
     $(this).removeClass("animated infinite pulse");
   }
 );
 
 //Added flash animation on hover to skills icons
 $("[id^=skills]").hover(
-  function () {
+  function() {
     $(this).addClass("animated infinite pulse");
   },
-  function () {
+  function() {
     $(this).removeClass("animated infinite pulse");
   }
 );
+
+//Hides and displays navbar after certain scroll
+$(document).ready(function() {
+  //Hide navbar initially
+  $(".nav-bar").hide();
+
+  // Fade animation
+  $(function() {
+    $(window).scroll(function() {
+      // Determines position on page before navbar displays
+      if ($(this).scrollTop() > 400) {
+        $(".nav-bar").fadeIn();
+      } else {
+        $(".nav-bar").fadeOut();
+      }
+    });
+  });
+});
 
 //Fades text into page
 function fadeIn(id) {
@@ -78,7 +103,7 @@ function fadeIn(id) {
 }
 
 //Timer on when to load the animation
-setTimeout(function () {
+setTimeout(function() {
   fadeIn("tag1");
   fadeIn("tag2");
   fadeIn("tag3");
