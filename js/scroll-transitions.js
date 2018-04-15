a7d34b3c8a0f665d29b3f0745723d23ef3857b73
Kwabena Boohene <okwabse@gmail.com>
2018-04-15 14:12:08 +0000

    Resolved hamburger transition

---
+++ js/scroll-transitions.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function($) {
+  /*Change dot navigation based on sections*/
+  $(window).scroll(() => {
+    //Section heights
+    let mT = $(".main").offset().top,
+      mH = $(".main").outerHeight();
+    let cdT = $(".cool-dev").offset().top;
+    let cfT = $(".code-fan").offset().top;
+    let rbT = $(".react-buff").offset().top;
+
+    //Window height
+    let wS = $(this).scrollTop() + 100;
+
+    if (wS >= cdT && wS < cfT) {
+      isCurrent("#li-dot2");
+    } else if (wS >= cfT && wS < rbT) {
+      isCurrent("#li-dot3");
+    } else if (wS >= rbT) {
+      isCurrent("#li-dot4");
+    } else {
+      isCurrent("#li-dot1");
+    }
+  });
+
+  //Checks for the current dot
+  function isCurrent(dotId) {
+    if ($(".current")[0]) {
+      let element = $(".dotstyle").find(".current");
+      element.removeClass("current");
+
+      $(dotId).addClass("current");
+    }
+  }
+
+  /*Scoll animation for different taglines*/
+
+  //cool-dev
+  $("#tag1, #dot2").click(function(e) {
+    e.preventDefault();
+    var position = $(".cool-dev").offset().top;
+    $("body, html").animate(
+      {
+        scrollTop: position
+      },
+      1200
+    );
+  });
+
+  //code-fanatic
+  $("#tag2, #dot3").click(function(e) {
+    e.preventDefault();
+    var position = $(".code-fan").offset().top;
+    $("body, html").animate(
+      {
+        scrollTop: position
+      },
+      1500
+    );
+  });
+
+  //react-buff
+  $("#tag3, #dot4").click(function(e) {
+    e.preventDefault();
+    var position = $(".react-buff").offset().top;
+    $("body, html").animate(
+      {
+        scrollTop: position
+      },
+      1800
+    );
+  });
+
+  //home
+  $("#dot1").click(function(e) {
+    e.preventDefault();
+    var position = $(".main").offset().top;
+    $("body, html").animate(
+      {
+        scrollTop: position
+      },
+      1800
+    );
+  });
+});
--- js/transitions.js
+++ js/transitions.js
@@ -11,67 +11,6 @@ jQuery(document).ready(function($) {
     );
   });
 
-  /*Scoll animation for different taglines*/
-
-  //cool-dev
-  $("#tag1, #dot2").click(function(e) {
-    e.preventDefault();
-    var position = $(".cool-dev").offset().top;
-    $("body, html").animate(
-      {
-        scrollTop: position
-      },
-      1200
-    );
-  });
-
-  //code-fanatic
-  $("#tag2, #dot3").click(function(e) {
-    e.preventDefault();
-    var position = $(".code-fan").offset().top;
-    $("body, html").animate(
-      {
-        scrollTop: position
-      },
-      1500
-    );
-  });
-
-  //react-buff
-  $("#tag3, #dot4").click(function(e) {
-    e.preventDefault();
-    var position = $(".react-buff").offset().top;
-    $("body, html").animate(
-      {
-        scrollTop: position
-      },
-      1800
-    );
-  });
-
-  //home
-  $("#dot1").click(function(e) {
-    e.preventDefault();
-    var position = $(".main").offset().top;
-    $("body, html").animate(
-      {
-        scrollTop: position
-      },
-      1800
-    );
-  });
-
-  //Scroll to top button
-  $("#arrow-up").click(() => {
-    $("html, body").animate(
-      {
-        scrollTop: 0
-      },
-      1800
-    );
-    return false;
-  });
-
   /*Hover animation for different items */
 
   //Added pulse animation on hover to social icons
@@ -122,46 +61,13 @@ jQuery(document).ready(function($) {
     fadeIn("tag2");
     fadeIn("tag3");
   }, 10);
+
+  /*Hamburger animation*/
+  $(".hamburger").click(() => {
+    if ($(".is-active")[0]) {
+      $(".hamburger").removeClass("is-active");
+    } else {
+      $(".hamburger").addClass("is-active");
+    }
+  });
 });
-
-/*Hamburger animation*/
-$(".hamburger").click(() => {
-  if ($(".is-active")[0]) {
-    $(this).removeClass("is-active");
-  } else {
-    $(this).addClass("is-active");
-  }
-});
-
-/*Change dot navigation based on sections*/
-$(window).scroll(() => {
-  //Section heights
-  let mT = $(".main").offset().top,
-    mH = $(".main").outerHeight();
-  let cdT = $(".cool-dev").offset().top;
-  let cfT = $(".code-fan").offset().top;
-  let rbT = $(".react-buff").offset().top;
-
-  //Window height
-  let wS = $(this).scrollTop() + 100;
-
-  if (wS >= cdT && wS < cfT) {
-    isCurrent("#li-dot2");
-  } else if (wS >= cfT && wS < rbT) {
-    isCurrent("#li-dot3");
-  } else if (wS >= rbT) {
-    isCurrent("#li-dot4");
-  } else {
-    isCurrent("#li-dot1");
-  }
-});
-
-//Checks for the current dot
-function isCurrent(dotId) {
-  if ($(".current")[0]) {
-    let element = $(".dotstyle").find(".current");
-    element.removeClass("current");
-
-    $(dotId).addClass("current");
-  }
-}
