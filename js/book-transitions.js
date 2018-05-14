2c4c6c9642af5180ef82436f787cf8c39a574d38
Kwabena Boohene <okwabse@gmail.com>
2018-05-14 20:29:51 +0000

    Added scroll navigation for book sections

--- js/book-transitions.js
+++ js/book-transitions.js
@@ -1,13 +1,15 @@
 jQuery(document).ready(function ($) {
+
+  //Fix navbar to the top based on scroll
   $(window).scroll(() => {
 
     let Ws = $(this).scrollTop();
     let bN = $('.book-list').offset().top;
     let fN = $('.footer').offset().top;
 
-    console.log("Ws :" + Ws);
-    console.log("bN :" + bN);
-    console.log("fN :" + fN);
+    // console.log("Ws :" + Ws);
+    // console.log("bN :" + bN);
+    // console.log("fN :" + fN);
 
     if (Ws >= bN && Ws < fN) {
       $(".book-nav").addClass('fixed-nav');
@@ -16,5 +18,27 @@ jQuery(document).ready(function ($) {
       $(".book-nav").removeClass('fixed-nav');
       $(".hidden-nav").removeClass('reveal');
     }
+  });
+
+  //Navigate to series book section
+  $("#sect-series").click(function (e) {
+    e.preventDefault();
+    var position = $("#series-sect").offset().top;
+    $("body, html").animate({
+        scrollTop: position
+      },
+      1500
+    );
+  });
+
+  //Navigate to singles book section
+  $("#sect-singles").click(function (e) {
+    e.preventDefault();
+    var position = $("#singles-sect").offset().top;
+    $("body, html").animate({
+        scrollTop: position
+      },
+      1500
+    );
   });
 });
