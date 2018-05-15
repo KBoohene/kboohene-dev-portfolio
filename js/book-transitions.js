b441cc8eb0cdc1291a1e35abbc61ba9c44a46a4c
Kwabena Boohene <okwabse@gmail.com>
2018-05-15 07:04:41 +0000

    Added animations to navbar text

--- js/book-transitions.js
+++ js/book-transitions.js
@@ -1,30 +1,51 @@
-jQuery(document).ready(function ($) {
-
+jQuery(document).ready(function($) {
   //Fix navbar to the top based on scroll
   $(window).scroll(() => {
-
     let Ws = $(this).scrollTop();
-    let bN = $('.book-list').offset().top;
-    let fN = $('.footer').offset().top;
+    let bN = $(".book-list").offset().top;
+    let fN = $(".footer").offset().top;
 
     // console.log("Ws :" + Ws);
     // console.log("bN :" + bN);
     // console.log("fN :" + fN);
 
     if (Ws >= bN && Ws < fN) {
-      $(".book-nav").addClass('fixed-nav');
-      $(".hidden-nav").addClass('reveal');
+      $(".book-nav").addClass("fixed-nav");
+      $(".hidden-nav").addClass("reveal");
     } else if (Ws < bN) {
-      $(".book-nav").removeClass('fixed-nav');
-      $(".hidden-nav").removeClass('reveal');
+      $(".book-nav").removeClass("fixed-nav");
+      $(".hidden-nav").removeClass("reveal");
+    }
+  });
+
+  //Navbar animations
+  $(window).scroll(() => {
+    let Ws = $(this).scrollTop() + 200;
+    let bN = $(".book-list").offset().top;
+
+    if (Ws >= bN) {
+      $("#nav-links").css("visibility", "visible");
+      $("#nav-links").removeClass("animated fadeOutDown");
+      $("#nav-links").addClass("animated fadeInUp");
+
+      $("#welcome-txt").css("visibility", "visible");
+      $("#welcome-txt").removeClass("animated fadeOutLeft");
+      $("#welcome-txt").addClass("animated fadeInLeft");
+    } else if (Ws < bN) {
+      $("#nav-links").removeClass("animated fadeInUp");
+      $("#nav-links").addClass("animated fadeOutDown");
+
+      $("#welcome-txt").removeClass("animated fadeInLeft");
+      $("#welcome-txt").addClass("animated fadeOutLeft");
     }
   });
 
   //Navigate to series book section
-  $("#sect-series").click(function (e) {
+  $("#sect-series").click(function(e) {
     e.preventDefault();
     var position = $("#series-sect").offset().top - 100;
-    $("body, html").animate({
+    $("body, html").animate(
+      {
         scrollTop: position
       },
       1500
@@ -32,10 +53,11 @@ jQuery(document).ready(function ($) {
   });
 
   //Navigate to singles book section
-  $("#sect-singles").click(function (e) {
+  $("#sect-singles").click(function(e) {
     e.preventDefault();
     var position = $("#singles-sect").offset().top - 100;
-    $("body, html").animate({
+    $("body, html").animate(
+      {
         scrollTop: position
       },
       1500
